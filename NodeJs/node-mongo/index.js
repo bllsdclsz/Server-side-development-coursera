c409a83963e871394d409b53b80a5d72db8b282c
ekolog <ekolog055@gmail.com>
2021-02-17 17:50:43 +0100

    callback hell and promise

--- NodeJs/node-mongo/index.js
+++ NodeJs/node-mongo/index.js
@@ -5,7 +5,7 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url).then((err, client) => {
+MongoClient.connect(url).then((client) => {
    console.log('Connected correctly to server');
 
    const db = client.db(dbname);
